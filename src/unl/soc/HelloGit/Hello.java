c4c7dff0e121a1d8052827a75e294404ab9b06f9
Logan Cole <lcole9@huskers.unl.edu>
2026-08-27 11:32:11 -0500

    Fix Date

--- src/unl/soc/HelloGit/Hello.java
+++ src/unl/soc/HelloGit/Hello.java
@@ -2,13 +2,14 @@ package unl.soc.HelloGit;
 
 /**
  * Author: Logan Cole (lcole9@huskers.unl.edu)
- * Date: 08/25/2026
+ * Date: 08/27/2026
  * 
  * A simple "Hello World!" program in Java, but in a git repo.
  * 
  * @author LoganColeUNL
  *
  */
+
 public class Hello {
 
 	public static void main(String[] args) {
